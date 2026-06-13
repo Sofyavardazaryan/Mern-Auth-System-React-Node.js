4f768e15dd1f9f9c55977b1af928d013186869b5
Sofya Vardazaryan <sofyavardazaryan@Sofyas-MacBook-Pro.local>
2026-06-13 13:15:39 +0400

    final commit

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,18 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 const generateAccessToken = (user) =>
-  jwt.sign(
-    { id: user._id, email: user.email },
-    process.env.ACCESS_SECRET,
-    { expiresIn: "15m" }
-  );
+  jwt.sign({ id: user._id, email: user.email }, process.env.ACCESS_SECRET, {
+    expiresIn: "15m",
+  });
 
 const generateRefreshToken = (user) =>
-  jwt.sign(
-    { id: user._id, email: user.email },
-    process.env.REFRESH_SECRET,
-    { expiresIn: "7d" }
-  );
+  jwt.sign({ id: user._id, email: user.email }, process.env.REFRESH_SECRET, {
+    expiresIn: "7d",
+  });
 
 module.exports = {
   generateAccessToken,
